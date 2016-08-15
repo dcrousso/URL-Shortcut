4d85f8121e8d8b92d00d0b61723999a0587f6d3d
Devin Rousso <dcrousso@gmail.com>
2016-08-14 23:24:45 -0700

    Add support for prefix matching

--- Chrome/js/background.js
+++ Chrome/js/background.js
@@ -5,6 +5,11 @@ chrome.storage.sync.get({
 		let redirect = redirects[url];
 		if (redirect)
 			return {redirectUrl: redirect};
+
+		for (let key of Object.keys(redirects)) {
+			if (url.startsWith(key))
+				return {redirectUrl: url.replace(key, redirects[key])};
+		}
 	}, {
 		urls: Object.keys(redirects).map(redirect => redirect + (redirect.endsWith("/") ? "*" : ""))
 	}, ["blocking"]);
